73e4ba2f8ad48a4dc659263b6948eb049153c4d9
subaash0123 <subaash0123@gmail.com>
2021-04-10 22:33:35 +0530

    Initial Commit

--- app/src/main/java/com/example/merchandiz/ConfirmFinalOrderActivity.java
+++ app/src/main/java/com/example/merchandiz/ConfirmFinalOrderActivity.java
@@ -1,7 +1,9 @@
 package com.example.merchandiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -9,8 +11,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+
+import Buyers.HomeActivity;
+import Buyers.ProductDetailsActivity;
 
 public class ConfirmFinalOrderActivity extends AppCompatActivity
 {
@@ -75,7 +86,54 @@ public class ConfirmFinalOrderActivity extends AppCompatActivity
         saveCurrentDate = currentDate.format(calForDate.getTime());
 
         SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
-        saveCurrentDate = currentDate.format(calForDate.getTime());
+        saveCurrentTime = currentDate.format(calForDate.getTime());
+
+        final DatabaseReference ordersRef = FirebaseDatabase.getInstance().getReference()
+                .child("Orders")
+                .child(Prevalent.currentOnlineUser.getPhone());
+
+
+
+        HashMap<String, Object> ordersMap = new HashMap<>();
+
+        ordersMap.put("totalAmount", totalAmount);
+        ordersMap.put("name", nameEditText.getText().toString());
+        ordersMap.put("phone", phoneEditText.getText().toString());
+        ordersMap.put("address", addressEditText.getText().toString());
+        ordersMap.put("city", cityEditText.getText().toString());
+        ordersMap.put("date", saveCurrentDate);
+        ordersMap.put("time", saveCurrentTime);
+        ordersMap.put("state", "not shipped");
+
+        ordersRef.updateChildren(ordersMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
+            {
+                if (task.isSuccessful())
+                {
+                    FirebaseDatabase.getInstance().getReference().child("Cart List")
+                            .child("User View")
+                            .child("User View")
+                            .child(Prevalent.currentOnlineUser.getPhone())
+                            .removeValue()
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task)
+                                {
+                                    if(task.isSuccessful())
+                                    {
+                                        Toast.makeText(ConfirmFinalOrderActivity.this,"Your final order has been placed successfully.",Toast.LENGTH_SHORT).show();
+
+                                        Intent intent = new Intent(ConfirmFinalOrderActivity.this, HomeActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
 
     }
 }
